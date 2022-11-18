c3fcd8e0de35f9596de60e5ac5b8a9a80c14cb42
Apple <IIKolchin@yandex.ru>
2022-11-18 13:51:15 +0300

    edit config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: './.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
@@ -15,7 +15,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Username/repository.git',
+      repo: 'https://github.com/IIKolchin/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
