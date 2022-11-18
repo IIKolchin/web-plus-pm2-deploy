d026b35f5e22843fb70b75c1ff84819ca3ea4dd9
Apple <IIKolchin@yandex.ru>
2022-11-18 18:46:57 +0300

    edit config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, GITHUB_USER, GITHUB_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: `https://github.com/${GITHUB_USER}/${GITHUB_REPO}.git`,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
